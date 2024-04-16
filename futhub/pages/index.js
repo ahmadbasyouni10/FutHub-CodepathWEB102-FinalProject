1539f126d85bbf26173fd764a0d3b3cd7a8815ee
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-16 01:29:36 -0400

    Setup futhub and installed dependencies

---
+++ futhub/pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="flex mt-4 mx-auto max-w-4xl border-4 border-emerald-500">
+      <div className="w-1/3">
+        <div className="border border-black">
+          links
+        </div>
+      </div>
+      <div className="">
+        <div className="border border-black">
+          form
+        </div>
+        post
+      </div>
+    </div>
+  );
+}
