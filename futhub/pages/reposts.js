081baaf59ff8ed0b103cf14a8216b8fc100aab9a
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-20 01:35:01 -0400

    Created frontend for reposts page, which uses post component

---
+++ futhub/pages/reposts.js
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout';
+import PostCard from '@/components/PostCard';
+
+const Reposts = () => {
+    return(
+        <div>
+            <Layout>
+                <h1 className='text-4xl mb-4 text-gray-500'>Reposts</h1>
+                <PostCard />
+                <PostCard />
+                <PostCard />
+            </Layout>
+        </div>
+    );
+    
+}
+
+export default Reposts;
