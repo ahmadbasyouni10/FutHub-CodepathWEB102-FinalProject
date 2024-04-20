bec3d15fd9aae8e9da6f6a2732c33bce031dfcb0
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-20 01:50:31 -0400

    Fixed avatar not taking to profile page

--- futhub/components/PostForm.js
+++ futhub/components/PostForm.js
@@ -2,16 +2,21 @@ import Card from "./Card";
 import { IoPersonOutline } from "react-icons/io5";
 import { IoLocationOutline } from "react-icons/io5";
 import Avatar from "./Avatar";
+import Link from "next/link";
 
 
 
 
 const PostForm = () => {
     return (
-        <Card>
+        <Card Padding={true}>
             <div className="flex items-center gap-3">
                 <div>
-                    <Avatar />
+                    <Link href="/profile">
+                        <span className="cursor-pointer">
+                            <Avatar big={false} />
+                        </span>
+                    </Link>
                 </div>
                 <textarea placeholder="What's on your Fut mind" className="w-full p-2 border border-gray-300 rounded-lg"></textarea>
             </div>
@@ -19,7 +24,7 @@ const PostForm = () => {
                 <button className="text-gray-500 fontpx-1 flex gap-1 items-center"><IoPersonOutline /> People</button>
                 <button className="text-gray-500 px-3 flex items-center"><IoLocationOutline /> Location</button>
                 <div className="grow text-right">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2">Post</button>
+                    <button className="bg-green-500 text-white px-4 py-2 rounded-lg mt-2">Post</button>
                 </div>
                 
 
