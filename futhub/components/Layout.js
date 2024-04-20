21a504c560790329d5e791e789c98f8444c3b2c7
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-20 01:46:54 -0400

    Refactored code on each page by creating layout component, which includes sidebar and children passed

--- futhub/components/Layout.js
+++ futhub/components/Layout.js
@@ -1,14 +1,14 @@
 import Sidebar from "./Sidebar";
-import PostForm from "./PostForm";
-import PostCard from "./PostCard";
 
-const Layout = ({children}) => {
+const Layout = ({children, nonavigation}) => {
         return (
             <div className="flex mt-4 mx-auto max-w-4xl gap-5">
+                {!nonavigation && 
                 <div className="w-1/4">
-                    <Sidebar />
-                </div>
-                <div className="w-3/4">
+                <Sidebar />
+            </div>
+            }
+                <div className={nonavigation ? "w-full" : "w-3/4"}>
                     {children}
                 </div>
             </div>
