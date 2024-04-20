c5396115aa1b3cee449c2af812dbea2c705f2619
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-20 01:39:57 -0400

    Fixed Avatar styling and passed prop big to control size of avatar on different pages

--- futhub/components/Avatar.js
+++ futhub/components/Avatar.js
@@ -1,6 +1,6 @@
-const Avatar = () => {
+const Avatar = ({big}) => {
     return (
-        <div className="w-14 h-14 overflow-hidden rounded-full">
+        <div className={`${big ? ' w-36 h-36' : 'w-14'} h-14 overflow-hidden rounded-full`}>
             <img className="object-cover w-full h-full" src="https://img.freepik.com/free-photo/emotions-people-concept-headshot-serious-looking-handsome-man-with-beard-looking-confident-determined_1258-26730.jpg" alt="Avatar"></img>
         </div>
     )
