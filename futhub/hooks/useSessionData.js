a997a635a73e8652bd117f6373910f4700b106a9
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-21 02:54:30 -0400

    Created hook, which will fetch session and store user's information from supabase table, returning pfp url and name for use in web app's pages

---
+++ futhub/hooks/useSessionData.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../client';
+
+export function useSessionData() {
+    const [session, setSession] = useState(null);
+    const [pfp, setPfp] = useState(null);
+    const [nameuser, setNameuser] = useState(null);
+    useEffect(() => {
+        const fetchSession = async () => {
+          const {data: { session },
+          } = await supabase.auth.getSession()
+          setSession(session);
+          supabase.from('users').select().eq('id', session.user.id)
+          .then((result) => {
+                console.log(result);
+            if (result.data.length > 0) {
+              setPfp(result.data[0].picture);
+              setNameuser(result.data[0].name);
+          }}
+        )};
+      fetchSession();
+      }, []);
+
+      return { pfp, nameuser}
+}
