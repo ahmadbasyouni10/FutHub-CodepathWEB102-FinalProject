5679e579426a09e2a20aed18e58116420adc2ab9
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-21 02:57:30 -0400

    Fixed error of user name not being available when component mounts, so added error handler letting user know profile is being fetched

--- futhub/components/PostCard.js
+++ futhub/components/PostCard.js
@@ -10,6 +10,7 @@ import { CiEdit } from "react-icons/ci";
 import { MdOutlineReport } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import Link from "next/link";
+import { useSessionData } from "../hooks/useSessionData";
 
 
 
@@ -22,30 +23,32 @@ const PostCard = () => {
         setShowDropdown(!showDropdown);
     }
 
+    const { pfp, nameuser} = useSessionData();
+
     return (
         <Card Padding={true}>
             <div className="flex gap-3">
                 <div className="cursor-pointer">
                     <Link href="/profile">
                         <span className="cursor-pointer">
-                            <Avatar big={false} />
+                            <Avatar url={pfp} big={false} />
                         </span>
                     </Link>
                 </div>
                 <div className="grow">
                     <h2 className="">
                         <Link href="/profile">
-                            <span className="cursor-pointer hover:underline font-semibold mr-1">John Doe</span>
+                            <span className="cursor-pointer hover:underline font-semibold mr-1">{nameuser}</span>
                         </Link>
-                        shared a <a className="text-green-500">post</a>
-                        <p className="text-gray-500">3 hours ago</p>
+                        shared a <a className="text-teal-500">post</a>
+                        <p className="text-gray-500 dark:text-gray-400">3 hours ago</p>
                     </h2>
                 </div>
                 <div>
                     <button className="text-gray-400 text-2xl" onClick={handleDropdown}><IoIosMore /></button>
                     <div className="relative">
                         {showDropdown && (
-                        <div className="absolute -right-7 bg-white p-3 rounded-sm border border-gray-100 w-40">
+                        <div className="absolute -right-7 bg-white dark:bg-dark p-3 rounded-sm border dark:border-gray-700 border-gray-100 w-40">
                             <a href="" className="items-center gap-3 flex py-2"><CiEdit /> Edit</a>
                             <a href="" className="items-center gap-3 flex py-2"><MdDeleteOutline /> Delete</a>
                             <a href="" className="items-center gap-3 flex py-2"><MdOutlineReport /> Report</a>
@@ -61,16 +64,16 @@ const PostCard = () => {
                 </div>
             </div>
             <div className='flex gap-3 mt-2'>
-                <button className="hover:bg-green-200 hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-gray-500 font-semibold flex gap-1 items-center"><BiUpvote /> 23</button>
-                <button className="hover:bg-green-200 hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-gray-500 font-semibold flex gap-1 items-center"><FaRegComment /> 4 </button>
-                <button className="hover:bg-green-200 hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-gray-500 font-semibold flex gap-1 items-center"><FaRegShareFromSquare /> Repost</button>
+                <button className="hover:bg-teal-200 hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-gray-500 font-semibold flex gap-1 dark:text-gray-400 items-center"><BiUpvote /> 23</button>
+                <button className="hover:bg-teal-200 hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-gray-500 font-semibold flex gap-1 dark:text-gray-400 items-center"><FaRegComment /> 4 </button>
+                <button className="hover:bg-teal-200 hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-gray-500 font-semibold flex gap-1 dark:text-gray-400 items-center"><FaRegShareFromSquare /> Repost</button>
             </div>
             <div className="flex mt-4 gap-3">
                 <div>
-                    <Avatar />
+                    <Avatar url={pfp} />
                 </div>
                 <div className="grow rounded-full relative">
-                    <textarea className="border block p-3 overflow-hidden px-4 h-12 rounded-full w-full" placeholder="Comment..."></textarea>
+                    <textarea className="border dark:border-gray-700 block p-3 outline-none dark:bg-dark overflow-hidden px-4 h-12 rounded-full w-full" placeholder="Comment..."></textarea>
                     <button className="absolute top-3 right-4 text-2xl"><IoImageOutline /></button>
                 </div>
             </div>
--- futhub/components/PostForm.js
+++ futhub/components/PostForm.js
@@ -3,28 +3,36 @@ import { IoPersonOutline } from "react-icons/io5";
 import { IoLocationOutline } from "react-icons/io5";
 import Avatar from "./Avatar";
 import Link from "next/link";
+import { useSessionData } from '../hooks/useSessionData';
+
 
 
 
 
 const PostForm = () => {
+    const { pfp, nameuser} = useSessionData();
+
+    if (!pfp || !nameuser ) {
+        return <div>Retreiving Profile...</div>
+    };
+    
     return (
         <Card Padding={true}>
             <div className="flex items-center gap-3">
                 <div>
                     <Link href="/profile">
                         <span className="cursor-pointer">
-                            <Avatar big={false} />
+                            <Avatar url={pfp} big={false} />
                         </span>
                     </Link>
                 </div>
-                <textarea placeholder="What's on your Fut mind" className="w-full p-2 border border-gray-300 rounded-lg"></textarea>
+                <textarea placeholder={`What's on your Fut mind, ${nameuser.split(' ')[0]}?`} className="w-full p-2 border border-gray-300 rounded-lg dark:text-black"></textarea>
             </div>
             <div className="flex items-center mt-3">
-                <button className="text-gray-500 fontpx-1 flex gap-1 items-center"><IoPersonOutline /> People</button>
-                <button className="text-gray-500 px-3 flex items-center"><IoLocationOutline /> Location</button>
+                <button className="text-gray-500 fontpx-1 dark:text-gray-400 flex gap-1 items-center"><IoPersonOutline /> People</button>
+                <button className="text-gray-500 px-3 dark:text-gray-400 flex items-center"><IoLocationOutline /> Location</button>
                 <div className="grow text-right">
-                    <button className="bg-green-500 text-white px-4 py-2 rounded-lg mt-2">Post</button>
+                    <button className="bg-teal-500 text-white dark:text-black px-4 py-2 rounded-lg mt-2">Post</button>
                 </div>
                 
 
