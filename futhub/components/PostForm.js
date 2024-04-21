b9a6794ec1f84f0fdec83c54aebbf58ab6da87d6
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-21 17:24:19 -0400

    Moved fetching of posts and user in postform, which allows user to post and store unique posts to user in database. Applied row level security on supabase

--- futhub/components/PostForm.js
+++ futhub/components/PostForm.js
@@ -3,17 +3,50 @@ import { IoPersonOutline } from "react-icons/io5";
 import { IoLocationOutline } from "react-icons/io5";
 import Avatar from "./Avatar";
 import Link from "next/link";
-import { useSessionData } from '../hooks/useSessionData';
-
-
-
+import { useEffect } from "react";
+import { useState } from "react";
+import { supabase } from "../client";
 
 
 const PostForm = () => {
-    const { pfp, nameuser} = useSessionData();
+    const [session, setSession] = useState(null);
+    const [pfp, setPfp] = useState(null);
+    const [nameuser, setNameuser] = useState(null);
+    const [postContent, setPostContent] = useState('');
+
+    const handleUserInput = (e) => {
+        setPostContent(e.target.value);
+    }
+
+    useEffect(() => {
+        const fetchSession = async () => {
+            const { data: { session } } = await supabase.auth.getSession();
+            setSession(session);
+            const { data: users, error } = await supabase.from('users').select().eq('id', session.user.id);
+            if (error) {
+                console.error('Error fetching user:', error);
+            } else if (users.length > 0) {
+                setPfp(users[0].picture);
+                setNameuser(users[0].name);
+            }
+        };
+        fetchSession();
+    }, []);
+
+    const createPost = async () => {
+        supabase.from('posts').insert({
+            postcontent: postContent,
+            creator: session.user.id
+        }).then((result) => {
+            console.log(result);
+            if (!result.error) {
+                alert('Post created successfully');
+            }
+        })
+}
 
     if (!pfp || !nameuser ) {
-        return <div>Retreiving Profile...</div>
+        return <div>Retrieving Profile...</div>
     };
     
     return (
@@ -26,13 +59,13 @@ const PostForm = () => {
                         </span>
                     </Link>
                 </div>
-                <textarea placeholder={`What's on your Fut mind, ${nameuser.split(' ')[0]}?`} className="w-full p-2 border border-gray-300 rounded-lg dark:text-black"></textarea>
+                <textarea value={postContent} onChange={handleUserInput} placeholder={`What's on your Fut mind, ${nameuser.split(' ')[0]}?`} className="w-full p-2 border border-gray-300 rounded-lg dark:text-black"></textarea>
             </div>
             <div className="flex items-center mt-3">
                 <button className="text-gray-500 fontpx-1 dark:text-gray-400 flex gap-1 items-center"><IoPersonOutline /> People</button>
                 <button className="text-gray-500 px-3 dark:text-gray-400 flex items-center"><IoLocationOutline /> Location</button>
                 <div className="grow text-right">
-                    <button className="bg-teal-500 text-white dark:text-black px-4 py-2 rounded-lg mt-2">Post</button>
+                    <button onClick={createPost} className="bg-teal-500 text-white dark:text-black px-4 py-2 rounded-lg mt-2">Post</button>
                 </div>
                 
 
