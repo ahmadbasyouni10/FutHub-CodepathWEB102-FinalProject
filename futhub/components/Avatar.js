8fefc4b223e82c0c1bc93833c70b35e118bf9bf5
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-21 02:55:27 -0400

    Accepts prop being url for pfp

--- futhub/components/Avatar.js
+++ futhub/components/Avatar.js
@@ -1,7 +1,7 @@
-const Avatar = ({big}) => {
+const Avatar = ({big, url}) => {
     return (
         <div className={`${big ? ' w-36 h-36' : 'w-14'} h-14 overflow-hidden rounded-full`}>
-            <img className="object-cover w-full h-full" src="https://img.freepik.com/free-photo/emotions-people-concept-headshot-serious-looking-handsome-man-with-beard-looking-confident-determined_1258-26730.jpg" alt="Avatar"></img>
+            <img className="object-cover w-full h-full" src={url} alt="Avatar"></img>
         </div>
     )
 };
