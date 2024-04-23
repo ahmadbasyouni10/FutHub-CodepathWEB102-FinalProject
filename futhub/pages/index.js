768ee9e8e9350ceef39295388e04213d96a1f291
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-22 23:43:05 -0400

    Fixed posts not being fetched automatically when making a post

--- futhub/pages/index.js
+++ futhub/pages/index.js
@@ -5,30 +5,43 @@ import { supabase } from "../client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Search from "@/components/Search";
+import Sidebar from "@/components/Sidebar";
 
 
 
 export default function Home() {
     const [session, setSession] = useState(null);
+    const [posts, setPosts] = useState([]);
     const router = useRouter();
+    <Sidebar session={session}/>
 
     useEffect(() => {
-      const fetchSession = async () => {
-        const {data: { session },
-        } = await supabase.auth.getSession()
-        setSession(session);
-      if (!session) {
-        router.push("/login");
-      }
-    };
-    fetchSession();
-    }, []);
+      fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const {data: { session },
+    } = await supabase.auth.getSession()
+    setSession(session);
+    if (!session) {
+      router.push('/login');
+    }
+    const { data: posts, error } = await supabase.from('posts').select('id, postcontent, photos, creator, created_at, users(id, picture, name)').order('created_at', {ascending: false});
+    if (error) {
+      console.error('Error fetching posts:', error);
+    } else if (posts.length > 0) {
+      setPosts(posts);
+    }
+  };
+      
 
     return (
-      <Layout>
-        <PostForm />
+      <Layout session={session}>
+        <PostForm onPost={fetchPosts} />
         <Search />
-        <PostCard />
+        {posts.map(post => (
+          <PostCard key={post.created_at} {...post} />
+        ))}
       </Layout>
     );
   }
