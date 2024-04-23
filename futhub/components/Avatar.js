393036aa660bf1b5988aac00dd909ab1ccb2c349
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-22 23:28:08 -0400

    Avatar reflects current user logged in postform, using session id of user

--- futhub/components/Avatar.js
+++ futhub/components/Avatar.js
@@ -1,6 +1,6 @@
 const Avatar = ({big, url}) => {
     return (
-        <div className={`${big ? ' w-36 h-36' : 'w-14'} h-14 overflow-hidden rounded-full`}>
+        <div className={`${big ? ' w-20 h-20' : 'w-14'} h-14 overflow-hidden rounded-full`}>
             <img className="object-cover w-full h-full" src={url} alt="Avatar"></img>
         </div>
     )
