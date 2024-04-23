b250dc626b4feb53ebe24ac6d1258f38374a0e7e
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-22 23:26:54 -0400

    Allow user to upload media, and delete them, which is reflected in supabase. Added row policies for authorization of each post. Implement delete and edit later on. Posting keeps track of time elapsed.

--- futhub/components/PostForm.js
+++ futhub/components/PostForm.js
@@ -1,18 +1,26 @@
 import Card from "./Card";
 import { IoPersonOutline } from "react-icons/io5";
 import { IoLocationOutline } from "react-icons/io5";
+import { MdPhotoLibrary } from "react-icons/md";
 import Avatar from "./Avatar";
 import Link from "next/link";
 import { useEffect } from "react";
 import { useState } from "react";
 import { supabase } from "../client";
+import { useRouter } from "next/router";  
+import ToolTip from "./ToolTip";
+import { ClipLoader } from "react-spinners";
+import { set } from "date-fns";
 
-
-const PostForm = () => {
+const PostForm = ({onPost}) => {
     const [session, setSession] = useState(null);
     const [pfp, setPfp] = useState(null);
     const [nameuser, setNameuser] = useState(null);
     const [postContent, setPostContent] = useState('');
+    const [isProcessing, setIsProcessing] = useState(false);
+    const [media, setMedia] = useState([]);
+    const router = useRouter();
+    
 
     const handleUserInput = (e) => {
         setPostContent(e.target.value);
@@ -22,12 +30,16 @@ const PostForm = () => {
         const fetchSession = async () => {
             const { data: { session } } = await supabase.auth.getSession();
             setSession(session);
+            if (!session) {
+                router.push('/login');
+                return
+              }
             const { data: users, error } = await supabase.from('users').select().eq('id', session.user.id);
             if (error) {
                 console.error('Error fetching user:', error);
             } else if (users.length > 0) {
-                setPfp(users[0].picture);
                 setNameuser(users[0].name);
+                setPfp(users[0].picture);
             }
         };
         fetchSession();
@@ -36,40 +48,90 @@ const PostForm = () => {
     const createPost = async () => {
         supabase.from('posts').insert({
             postcontent: postContent,
-            creator: session.user.id
-        }).then((result) => {
-            console.log(result);
-            if (!result.error) {
-                alert('Post created successfully');
-            }
+            creator: session.user.id,
+            photos: media
+        }).then(() => {
+            setPostContent('');
+            setMedia([]);
+            onPost();
         })
 }
 
     if (!pfp || !nameuser ) {
         return <div>Retrieving Profile...</div>
     };
+
+    const handleMedia = async(e) => {
+        const files = e.target.files;
+        if (files.length > 0) {
+            setIsProcessing(true);
+            for (let i = 0; i < files.length; i++) {
+                const newFileName = `${session.user.id}-${Date.now()}${files[i].name}`;
+                const result = await supabase.storage.from('photos').upload(newFileName, files[i])
+                        if (result.data) {
+                            const url = process.env.NEXT_PUBLIC_supabaseURL + '/storage/v1/object/public/photos/' + result.data.path
+                            setMedia(prevMedia => [...prevMedia, url]);
+                    }
+                setIsProcessing(false)
+                ;
+            }
+        };
+    }
     
     return (
         <Card Padding={true}>
             <div className="flex items-center gap-3">
                 <div>
-                    <Link href="/profile">
+                    <Link href={"/profile/" + session.user.id}>
                         <span className="cursor-pointer">
                             <Avatar url={pfp} big={false} />
                         </span>
                     </Link>
                 </div>
-                <textarea value={postContent} onChange={handleUserInput} placeholder={`What's on your Fut mind, ${nameuser.split(' ')[0]}?`} className="w-full p-2 border border-gray-300 rounded-lg dark:text-black"></textarea>
+                <textarea value={postContent} onChange={handleUserInput} placeholder={`What's on your Fut mind, ${nameuser.split(' ')[0]}?`} className="w-full dark:bg-dark dark:text-white p-2 border border-gray-300 rounded-lg "></textarea>
             </div>
-            <div className="flex items-center mt-3">
-                <button className="text-gray-500 fontpx-1 dark:text-gray-400 flex gap-1 items-center"><IoPersonOutline /> People</button>
-                <button className="text-gray-500 px-3 dark:text-gray-400 flex items-center"><IoLocationOutline /> Location</button>
-                <div className="grow text-right">
-                    <button onClick={createPost} className="bg-teal-500 text-white dark:text-black px-4 py-2 rounded-lg mt-2">Post</button>
-                </div>
-                
+            {isProcessing && (
+            <div className="mt-2">
+                <ClipLoader color='#009688' speedMultiplier={1} loading={isProcessing} size={30} />
+            </div>
+            )}
+            {media.length > 0 && (
+    <div className="flex gap-1 max-w-30">
+        {media.map((url, index) => (
+            <div className="relative mt-2">
+                <img src={url} alt="" className="mt-2 rounded-md w-auto h-16" />
+                <button 
+                    className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center" 
+                    onClick={() => {
+                        const newMedia = [...media];
+                        newMedia.splice(index, 1);
+                        setMedia(newMedia);
+                    }}
+                >
+                    x
+                </button>
+            </div>
+        ))}
+    </div>
+)}
 
+            <div className="flex items-center mt-3 justify-between">
+                    <div className="flex items-center gap-3">
+                <ToolTip content="Person">
+                    <button className="text-gray-500 fontpx-1 dark:text-gray-400 flex gap-1 text-2xl items-center"><IoPersonOutline /></button>
+                </ToolTip>
+                <ToolTip content="Location">
+                    <button className="text-gray-500 px-3 dark:text-gray-400 text-2xl flex items-center"><IoLocationOutline /></button>
+                </ToolTip>
+                <ToolTip content="Media">
+                    <label htmlFor="media" className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"><MdPhotoLibrary /></label>
+                    <input type="file" id="media" className="hidden" multiple onChange={handleMedia} />
+                </ToolTip>
             </div>
+            <div className="text-right">
+                <button onClick={createPost} className="bg-teal-500 text-white dark:text-black px-4 py-2 rounded-lg mt-2">Post</button>
+            </div>
+        </div>
         </Card>
     );
     
