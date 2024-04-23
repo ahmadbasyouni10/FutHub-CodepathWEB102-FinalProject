8728e360902477f60c9d0eb65ef22431074cca98
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-22 23:37:37 -0400

    fixed styling

--- futhub/components/Layout.js
+++ futhub/components/Layout.js
@@ -1,6 +1,7 @@
 import Sidebar from "./Sidebar";
 import Card from "./Card";
 import Avatar from "./Avatar";
+import Recommended from "./Recommended";
 
 const Layout = ({children, nonavigation}) => {
         return (
@@ -8,27 +9,7 @@ const Layout = ({children, nonavigation}) => {
                 {!nonavigation && 
                 <div className=" w-4/12">
                     <Sidebar />
-                    <Card Padding={true}>
-                        <h2 className="text-2xl text-gray-400 font-bold">Who To follow</h2>
-                        <div className='gap-3 mt-3'>
-                            <div className="gap-2 flex items-center py-2">
-                                <Avatar />
-                                <h3 className='font-bold text-center'>Katherine</h3>
-                            </div>
-                            <div className="gap-2 flex items-center py-2">
-                                <Avatar />
-                                <h3 className='font-bold text-center'>Katherine</h3>
-                            </div>
-                            <div className="gap-2 flex items-center py-2">
-                                <Avatar />
-                                <h3 className='font-bold text-center'>Katherine</h3>
-                            </div>
-                            <div className="gap-2 flex items-center py-2">
-                                <Avatar />
-                                <h3 className='font-bold text-center'>Katherine</h3>
-                            </div>
-                        </div>
-                    </Card>
+                    <Recommended />
                 </div> 
                 }
                 
