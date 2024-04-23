9cd905a12aca0953559c65e34cfc55a96a930ad4
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-22 23:49:11 -0400

    Refactored code for profile page

--- futhub/pages/profile/[...profile].js
+++ futhub/pages/profile/[...profile].js
@@ -1,5 +1,8 @@
 import ProfilePage from "../profile";
+import { useRouter } from "next/router";
 
 export default function Profile() {
+    const router = useRouter();
+    console.log(router)
     return <ProfilePage />
 } 
