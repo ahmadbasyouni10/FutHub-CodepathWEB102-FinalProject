c4aeec4fd3dcc85d9f8297c853162cdcb8d6c58e
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-25 12:49:37 -0400

    Fixed path to profile by adding check for session

--- futhub/components/Sidebar.js
+++ futhub/components/Sidebar.js
@@ -9,10 +9,13 @@ import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
 import { supabase } from "../client";
 import ThemeToggle from "./ThemeToggle";
+import { useEffect, useState } from "react";
+
 
 
 const Sidebar = () => {
     const router = useRouter();
+    const [session, setSession] = useState(null);
     const activeItemStyles = 'text-white dark:text-black bg-teal-500 hover:bg-teal-200'
     const {asPath:currentPath} = router;
     const handleLogOut = async () => {
@@ -21,12 +24,25 @@ const Sidebar = () => {
         else router.push('/login');
     }
 
+    useEffect(() => {
+        const fetchSession = async () => {
+            const { data: { session } } = await supabase.auth.getSession();
+            setSession(session);
+            if (!session) {
+                router.push('/login');
+                return
+              }
+        };
+        fetchSession();
+    }, []);
+
+
     return(
         <div>
             <Card Padding={true} className="">
             <h2 className=" text-gray-400 text-2xl font-bold mb-3 pt-2">Futhub</h2>
             <Link href="/" className={`${currentPath === '/' ? activeItemStyles : 'hover:bg-teal-200'} hover:scale-110 hover:bg-opacity-50 text-lg hover:shadow-md flex items-center h-10 gap-2 py-5 -mx-4 px-4 rounded-md transition-all`}><TiHomeOutline className="text-2xl" />Home</Link>
-            <Link href={"/profile"} className={`${currentPath === '/profile' || currentPath === '/profile/about' || currentPath === '/profile/posts' ? activeItemStyles : 'hover:bg-teal-200'} hover:scale-110 hover:bg-opacity-50 hover:shadow-md flex items-center text-lg h-10 gap-2 py-5 -mx-4 px-4 rounded-md transition-all`}><CgProfile className="text-2xl" />Profile</Link>
+            <Link href={"/profile/" + session?.user.id + '/posts'} className={`${currentPath === '/profile' || currentPath === '/profile/about' || currentPath === '/profile/posts' ? activeItemStyles : 'hover:bg-teal-200'} hover:scale-110 hover:bg-opacity-50 hover:shadow-md flex items-center text-lg h-10 gap-2 py-5 -mx-4 px-4 rounded-md transition-all`}><CgProfile className="text-2xl" />Profile</Link>
             <Link href="/profile/friends" className={`${currentPath === '/profile/friends' ? activeItemStyles : 'hover:bg-teal-200'} hover:scale-110 hover:bg-opacity-50 hover:shadow-md text-lg flex items-center h-10 gap-2 py-5 -mx-4 px-4 rounded-md transition-all`}><LiaUserFriendsSolid className="text-2xl" />Friends</Link>
             <Link href="/reposts" className={`${currentPath === '/reposts' ? activeItemStyles : 'hover:bg-teal-200'} hover:scale-110 hover:bg-opacity-50 hover:shadow-md flex text-lg items-center h-10 gap-2 py-5 -mx-4 px-4 rounded-md transition-all`}><BiRepost className="text-2xl" />Reposts</Link>
             <Link href="/notifications" className={`${currentPath === '/notifications' ? activeItemStyles : 'hover:bg-teal-200'} hover:scale-110 text-lg hover:bg-opacity-50 hover:shadow-md flex items-center h-10 gap-2 py-5 -mx-4 px-4 rounded-md transition-all`}><IoMdNotificationsOutline className="text-2xl" />Notifications</Link>
