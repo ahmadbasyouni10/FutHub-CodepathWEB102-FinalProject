fd2df8ffa92c379e5ec4c1c603187080276f8ae2
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-26 16:56:13 -0400

    Allow users to search posts and display results as input changes

--- futhub/components/Search.js
+++ futhub/components/Search.js
@@ -1,11 +1,21 @@
-const Search = () => {
+// Search.js
+import { useState } from 'react';
+
+const Search = ({onSearch}) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+        onSearch(term);
+    }
+
     return (
         <div className="flex gap-2 p-6 items-center">
-            <input type="text" className="px-5 dark:bg-dark py-1 w-1/3 sm:px-5 sm:py-3 flex-1 bg-gray-200 rounded-full focus:outline-none placeholder:text-zinc-400" placeholder="What are you looking for?" />
-            <button className="bg-teal-500 text-white px-4 py-2 dark:text-black rounded-lg">Search</button>
+            <input type="text" className="px-5 dark:bg-dark py-1 w-1/3 sm:px-5 sm:py-3 flex-1 bg-gray-200 rounded-full focus:outline-none placeholder:text-zinc-400" placeholder="What are you looking for?"
+            value={searchTerm} onChange={handleInputChange} />
         </div>
     )
-
 }
 
 export default Search;
