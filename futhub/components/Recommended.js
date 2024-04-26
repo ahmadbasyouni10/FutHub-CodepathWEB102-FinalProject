4983429d44a20f42d81a67fdac3accc402fb25d4
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-26 16:58:35 -0400

    Shows first 4 users on website to discover new people

--- futhub/components/Recommended.js
+++ futhub/components/Recommended.js
@@ -2,32 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { supabase } from '../client';
 import Card from './Card';
 import Avatar from './Avatar';
-
+import Link from 'next/link';
 
 
 const Recommended = () => {
     const [session, setSession] = useState(null);
     const [users, setUsers] = useState([]);
+    const [randomUsers, setRandomUsers] = useState([]);
+    
     useEffect(() => {
         const fetchUsers = async () => {
           const {data: { session },
           } = await supabase.auth.getSession()
           supabase.from('users').select().then((result) => {
             if (result.data.length > 0) {
-                setUsers(result.data);
+                setRandomUsers(result.data.slice(0,4));
               
           }}
         )};
       fetchUsers();
       }, []);
+
       return (
         <Card Padding={true}>
                         <h2 className="text-2xl text-gray-400 font-bold">Who To follow</h2>
                         <div className='gap-3 mt-3'>
-                            {users.map(user => (
+                            {randomUsers.map(user => (
                                 <div className="gap-2 flex items-center py-2">
+                                    <Link  className='flex items-center gap-2 py-2' href={"/profile/"+user?.id+'/posts'}>
                                     <Avatar url={user.picture} />
                                     <h3 className='font-bold text-center'>{user.name}</h3>
+                                    </Link>
                                 </div>
                             ))}
                         </div>
@@ -38,3 +43,4 @@ const Recommended = () => {
 }
 
 export default Recommended;
+
